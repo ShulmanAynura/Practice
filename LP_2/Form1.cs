2773548041dd3952f09a0bf974138ff652ea64d6
Alexandr <Alexandr@BELKAPC>
2017-07-08 15:52:23 +0300

    Add LP

---
+++ LP_2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LP_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TomitaWrapper t = new TomitaWrapper();
+            t.FindFact();
+
+            
+
+            richTextBox1.Clear();
+            //string fileName = Path.Combine(Properties.Settings.Default.SourceFolder, "output.txt");
+            string fileName = "output.txt";
+            if (File.Exists(fileName))
+            {
+                var sr = new StreamReader(fileName, Encoding.UTF8);
+                string text = sr.ReadToEnd();
+                richTextBox1.AppendText(text);
+                sr.Close();
+            }
+        }
+    }
+}
---
+++ LP_2/TomitaWrapper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace LP_2
+{
+    class TomitaWrapper
+    {
+        public void FindFact()
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                //имя файла
+                FileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                                        Path.Combine(Properties.Settings.Default.SourceFolder,
+                                                Properties.Settings.Default.TomitaFileName)),
+                //скрытое окно
+                //WindowStyle = ProcessWindowStyle.Hidden,
+                //ваши аргументы
+               Arguments = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                                        Path.Combine(Properties.Settings.Default.SourceFolder, "config.proto"))
+            };
+            //запуск процесса
+            startInfo.WorkingDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                                       Properties.Settings.Default.SourceFolder);
+            startInfo.UserName = null;
+            //Process.Start(startInfo);
+
+            string f = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                                        Path.Combine(Properties.Settings.Default.SourceFolder,
+                                                Properties.Settings.Default.TomitaFileName));
+            string arg = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                                        Path.Combine(Properties.Settings.Default.SourceFolder, "config.proto"));
+
+            Process pr = new Process();
+            pr.StartInfo = new ProcessStartInfo("tomitaparser.exe", "config.proto");
+            pr.Start();
+            pr.WaitForExit();//ожидание завершения
+           
+            //удаление лишних файлов
+            //File.Delete("first.bin");
+            //File.Delete("dic.gzt.bin");
+            //File.Delete("output.txt");
+
+        }
+    }
+}
